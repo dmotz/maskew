6a46392d75d67d9b9ce5f4da5105e64c149b85a1
dmotz <motzdc@gmail.com>
2012-12-16 00:16:34 -0500

    updated demo files

--- demo/demo.js
+++ demo/demo.js
@@ -4,11 +4,10 @@
             demos = document.getElementsByClassName('demo');
 
         for(var i = 0, l = letters.length; i < l; i++){
-            new Maskew(letters[i], 6, { interactive: true, anchor: 'bottom', showElement: 'inline-block' });
+            new Maskew(letters[i], 6, { touch: true, anchor: 'bottom', showElement: 'inline-block' });
         }
-
         for(i = 0, l = demos.length; i < l; i++){
-            new Maskew(demos[i], 8, { interactive: true, anchor: 'bottom' });
+            new Maskew(demos[i], 12, { touch: true });
         }
 
     }, false);
